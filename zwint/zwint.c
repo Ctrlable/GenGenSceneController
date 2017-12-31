4d835b4af2bfe83c1978ecf155dfe2fc1ac38a5c
ElephantEight <34627382+ElephantEight@users.noreply.github.com>
2017-12-31 10:38:37 -0800

    1.04 with GPL 2.0 license
    Change license to GPL 2.0. 
    Also, any plug-ins which use Zwint must also be at least GPL 2.0

--- zwint/zwint.c
+++ zwint/zwint.c
@@ -1,17 +1,24 @@
 /*
  * ZwInt - Z-Wave data monitor/interceptior for Vera
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Copyright (C) 2017  Gustavo A Fernandez
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License (GPL)
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You can access the terms of this license at
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ *
+ * Furthermore, any plug-ins or apps which use this program must
+ * also be licensed under the terms of the GPL 2.0 or later
+ * or another compatible license.
  */
 
 #include <dirent.h>
